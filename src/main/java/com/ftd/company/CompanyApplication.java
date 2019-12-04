2dc662ac8ff9dacfd1f9665a76791e8bbbc5931f
Yun Fei Bai <byfbaish@cn.ibm.com>
2019-12-04 16:55:53 +0900

    配置更改

--- src/main/java/com/ftd/company/CompanyApplication.java
+++ src/main/java/com/ftd/company/CompanyApplication.java
@@ -2,9 +2,11 @@ package com.ftd.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CompanyApplication {
 
